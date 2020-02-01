a7ef4c1cad5267732f33d448cf5e29ecde7ff793
Loveschach <codymalach@gmail.com>
2020-01-31 22:45:37 -0800

    Mostly checking in so Devin can get the scripts folder

---
+++ Assets/Scripts/Bug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bug : MonoBehaviour {
+	bool bugFixed = false;
+	public int ID;
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	public void SetBugFixed( bool bugFixed ) {
+		this.bugFixed = bugFixed;
+	}
+}
---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveSystem
+{
+	public static void Save() {
+
+	}
+
+	/*public static bool GetBugState( int bugID ) {
+		PlayerPrefs
+	}*/
+}
