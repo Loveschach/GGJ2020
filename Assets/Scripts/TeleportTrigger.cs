8b0d95578a2abd55f1aa970f1b9f043e55a9546b
nadescape <2817458+nadescape@users.noreply.github.com>
2020-02-01 15:10:29 -0800

    level design wip - stairs and first platform room done

---
+++ Assets/Scripts/TeleportTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class TeleportTrigger : MonoBehaviour
+{
+	public GameObject teleportOrigin;
+    
+    void OnTriggerEnter( Collider other )
+    {
+    	var controller = other.GetComponent<FirstPersonController>();
+    	StartCoroutine( TeleportCharacter( controller ) );
+    }
+
+    IEnumerator TeleportCharacter( FirstPersonController controller )
+ 	{
+ 
+    	controller.enabled = false;
+    	controller.transform.position = teleportOrigin.transform.position;
+    	yield return new WaitForSeconds(0.05f);
+    	controller.enabled = true; 
+ 	}
+}
