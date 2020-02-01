1a7f0612da515021d2b3ad4a27cabb64c80d0d0c
nadescape <2817458+nadescape@users.noreply.github.com>
2020-02-01 13:45:08 -0800

    check in bug class for devin

--- Assets/Scripts/Bug.cs
+++ Assets/Scripts/Bug.cs
@@ -3,7 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bug : MonoBehaviour {
+	public enum LogStatus
+	{
+		Future,
+		Undiscovered,
+		Assigned,
+		InProgress,
+		Fixed,
+		NotFixed
+	}
 	public bool logged = false;
+	public LogStatus status = LogStatus.Future;
+	public int buildIntroduced = 0; // one build per day
 	public int ID;
 	// Start is called before the first frame update
 	void Start() {
