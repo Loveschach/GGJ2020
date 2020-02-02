9563eb9b5a57573937e18c992aaf5fc73175afa1
Devin Curry - Laptop <Devination3D@gmail.com>
2020-02-02 14:17:37 -0800

    random background sounds

---
+++ Assets/Scripts/RandomSounds.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSounds : MonoBehaviour
+{
+	public float minRandTime = 0.1f;
+	public float maxRandTime = 0.5f;
+	public AudioClip[] sounds;
+	AudioSource audioSource;
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+		StartCoroutine("PlayRandomSound");
+	}
+
+	IEnumerator PlayRandomSound() {
+		yield return new WaitForSeconds(Random.Range(minRandTime, maxRandTime));
+		if (!audioSource.isPlaying) {
+			Utils.PlayRandomAudio(audioSource, sounds);
+		}
+		StartCoroutine("PlayRandomSound");
+	}
+}
