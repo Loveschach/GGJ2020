27e1d94c844251c20e411173ad3e78f109925258
nadescape <2817458+nadescape@users.noreply.github.com>
2020-02-01 23:46:50 -0800

    enemies rush player on sight

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 	GameObject player;
+	bool moving = false;
+
+	public bool seekPlayer;
+	public float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +20,55 @@ public class Enemy : MonoBehaviour
     void Update()
     {
         FacePlayer();
+        if ( ShouldMove() )
+        {
+        	StartCoroutine( "Booove" );
+        }
     }
 
     void FacePlayer()
     {
-    	
+    	transform.LookAt( player.transform );
+    	transform.rotation = Quaternion.Euler( 0, transform.rotation.eulerAngles.y, 0 );
+    }
+
+    bool ShouldMove()
+    {
+    	if ( moving )
+    		return false;
+
+    	RaycastHit hit;
+
+    	Physics.Raycast( transform.position, player.transform.position - transform.position, out hit, 100 );
+
+    	if ( hit.transform == null )
+    		return false;
+
+    	if ( hit.transform.gameObject != player )
+    		return false;
+
+    	return true;
+    }
+
+    IEnumerator Booove()
+    {
+    	moving = true;
+
+    	// TODO: ghost screech
+    	yield return new WaitForSeconds( 0.5f );
+
+    	var direction = transform.forward;
+    	if ( seekPlayer )
+    		direction = player.transform.position - transform.position + new Vector3( 0, 2, 0 );
+
+    	direction = Vector3.Normalize( direction );
+
+    	var interval = 0.05f;
+
+    	for ( int i = 0; i < 1000; i++ )
+    	{
+    		transform.position += direction * speed * interval;
+    		yield return new WaitForSeconds( interval );
+    	}
     }
 }
