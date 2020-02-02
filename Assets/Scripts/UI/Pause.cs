3c1ab27d9731f4d834765a756e29bc1b3e78013c
Loveschach <codymalach@gmail.com>
2020-02-02 11:59:12 -0800

    Add slightly busted pause menu.
    For some reason mouse look doesn't freeze when time scale is 0.

---
+++ Assets/Scripts/UI/Pause.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+	public GameObject pauseScreen;
+	PauseMenu pauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+		pauseMenu = pauseScreen.GetComponent<PauseMenu>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if ( Input.GetButtonDown( "Cancel" ) ) {
+			if ( pauseScreen.activeSelf ) {
+				pauseMenu.Resume();
+			}
+			else {
+				Time.timeScale = 0;
+				Cursor.lockState = CursorLockMode.None;
+				Cursor.visible = true;
+				pauseScreen.SetActive( true );
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
+
+	public void Resume() {
+		Time.timeScale = 1;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		gameObject.SetActive( false );
+	}
+
+	public void EndLevel() {
+		//TODO: Stub
+	}
+
+	public void Exit() {
+		Application.Quit();
+	}
+
+	// Update is called once per frame
+	void Update() {
+	}
+}
