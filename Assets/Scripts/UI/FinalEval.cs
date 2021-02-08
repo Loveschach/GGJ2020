8c47e406d8ee07e83d62673bcc689dea2d214bd6
Loveschach <codymalach@gmail.com>
2021-02-07 20:56:55 -0800

    Update final eval.

--- Assets/Scripts/UI/FinalEval.cs
+++ Assets/Scripts/UI/FinalEval.cs
@@ -6,20 +6,21 @@ using UnityEngine.UI;
 public class FinalEval : MonoBehaviour
 {
 	static int SPLASH_1_TIME = 3;
-	static int SPLASH_2_TIME = 9;
+	static int SPLASH_2_TIME = 13;
 	static int SPLASH_3_TIME = 3;
 	static int PRE_EXIT_TIME = 3;
-	static string EPILOGUE_BAD = "Critics hate your game.\nIt's basically unplayable because of bugs.\nAdrian J. was fired.\nxXbsp_mstrXx died.\ndrawMe1ofUrFrnchGrlz broke their pencil.\nCodeNinja got a pay raise.";
+	static string EPILOGUE_BAD = "Critics hate your game.\nIt's basically unplayable because of bugs.\nAdrian J. was fired.\nxXbsp_mstrXx died.\ndrawMe1ofUrFrnchGrlz's tablet broke.\nCodeNinja got a pay raise.";
 	static string META_BAD = "79%";
 	static string EPILOGUE_OK = "Critics think your game is just fine.\nThere are bugs, but not too many.\nAdrian J. stayed at their job for 20 years.\nxXbsp_mstrXx's level was mentioned in 5 reviews.\ndrawMe1ofUrFrnchGrlz got 1000 new UsTube followers.\nCodeNinja ate some rotten turkey.";
 	static string META_OK = "81%";
-	static string EPILOGUE_GOOD = "Your game is the biggest thing since GYE V!\nNobody ran into any bugs ever.\nAdrian J. quit their job and started a succesful indie studio.\nxXbsp_mstrXx and drawMe1ofUrFrnchGrlz got married.\nCodeNinja took a sabbatical in Spain and was never seen again.";
+	static string EPILOGUE_GOOD = "Your game is the biggest thing since GYE V!\nNo bugs were ever found.\nAdrian J. quit their job and started a succesful indie studio.\nxXbsp_mstrXx and drawMe1ofUrFrnchGrlz got married.\nCodeNinja took a sabbatical in Spain and was never seen again.";
 	static string META_GOOD = "95%";
 	public GameObject splash1;
 	public GameObject splash2;
 	public GameObject splash3;
 	public GameObject final;
 	public GameObject exitButton;
+	public AudioSource Music;
 	int totalResolvedBugs;
 	enum FinalState {
 		SPLASH_1,
@@ -55,10 +56,10 @@ public class FinalEval : MonoBehaviour
 		}
 
 		Text splash2Text = splash2.transform.Find( "Epilogue" ).GetComponent<Text>();
-		if ( totalResolvedBugs >= 11 ) {
+		if ( totalResolvedBugs >= 38 ) {
 			splash2Text.text = EPILOGUE_GOOD;
 		}
-		else if ( totalResolvedBugs >= 5 ) {
+		else if ( totalResolvedBugs >= 30 ) {
 			splash2Text.text = EPILOGUE_OK;
 		} 
 		else {
@@ -81,6 +82,7 @@ public class FinalEval : MonoBehaviour
 		switch ( currentState ) {
 			case ( FinalState.SPLASH_1 ):
 				if( currentTime >= SPLASH_1_TIME ) {
+					Music.Play();
 					splash1.SetActive( false );
 					splash2.SetActive( true );
 					currentState = FinalState.SPLASH_2;
